4ecf79a4d8c6278d392c5068f918e0835131ce81
Erick <erickkirui653@gmail.com>
2023-05-17 13:08:18 -0400

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+  fib_list = []
+  a, b = 0, 1
+  
+  for _ in range(length):
+      fib_list.append(a)
+      a, b = b, a + b
+  
+  
+  #fib_list.append(0)
+  print(fib_list)
+  
+  
+  
